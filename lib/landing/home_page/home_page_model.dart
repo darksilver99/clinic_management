9f0a6e8f3996bdab55502fc02b034d8f2563662a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-21 08:50:55 +0000

    Updating to latest FlutterFlow output.

--- lib/landing/home_page/home_page_model.dart
+++ lib/landing/home_page/home_page_model.dart
@@ -4,6 +4,17 @@ import 'home_page_widget.dart' show HomePageWidget;
 import 'package:flutter/material.dart';
 
 class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+  ///  Local state fields for this page.
+
+  List<double> duration = [200.0, 400.0, 600.0, 800.0];
+  void addToDuration(double item) => duration.add(item);
+  void removeFromDuration(double item) => duration.remove(item);
+  void removeAtIndexFromDuration(int index) => duration.removeAt(index);
+  void insertAtIndexInDuration(int index, double item) =>
+      duration.insert(index, item);
+  void updateDurationAtIndex(int index, Function(double) updateFn) =>
+      duration[index] = updateFn(duration[index]);
+
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
